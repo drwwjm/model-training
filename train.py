aea92d3be6e654f9856ba212e7f8afe4e9fe329a
Drew <160739722+drwwjm@users.noreply.github.com>
2024-05-15 19:14:35 +0800

    new medium 100

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 if __name__ ==  "__main__":
     model = YOLO('yolov8m.pt')
 
-    results = model.train(data=r'D:\DRIVE\TUP Files\4A\Thesis\model\yolo\data.yaml', epochs=100, imgsz=640, project=r'D:\DRIVE\TUP Files\4A\Thesis\model\yolo\results', device=0, workers=0, optimize= True, lr0=0.001, lrf=0.01, fliplr=0.0,amp=False, batch=2)
+    results = model.train(data=r'D:\DRIVE\TUP Files\4A\Thesis\model\yolo\data.yaml', epochs=80, imgsz=640, project=r'D:\DRIVE\TUP Files\4A\Thesis\model\yolo\results', device=0, workers=0, optimize= True, lr0=0.001, lrf=0.01, fliplr=0.0,batch=4 ,amp=False)
